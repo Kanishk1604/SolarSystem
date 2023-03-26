6004fbee41778df314e2dae70aebb553cc66958f
Kanishk Saini <117044498+Kanishk1604@users.noreply.github.com>
2023-03-26 18:52:27 -0400

    Update Assignment3KS.java
    I have still to complete the Keyboard interaction

--- Assignment3KS.java
+++ Assignment3KS.java
@@ -16,16 +16,54 @@ import org.jogamp.java3d.utils.image.TextureLoader;
 import org.jogamp.java3d.utils.universe.SimpleUniverse;
 import org.jogamp.vecmath.*;
 
-public class Assignment3KS extends JPanel {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+
+public class Assignment3KS extends JPanel implements KeyListener {
   
     
 	private static final long serialVersionUID = 1L;
 	private static JFrame frame;
 	private static final int OBJ_NUM = 1;
+	protected static Alpha alpha1;                           //this is for revolving
+	protected static Alpha alpha2;                           //this is for revolving
+	protected static Alpha alpha3;                           //this is for revolving
+	protected static Alpha alpha4;                           //this is for revolving
+	protected static Alpha alpha5;                           //this is for revolving
+	protected static Alpha alpha6;                           //this is for revolving
+	protected static Alpha alpha7;                           //this is for revolving
+	protected static Alpha alpha8;                           //this is for revolving
+	protected static Alpha alpha9;                           //this is for revolving
+	
+	protected static Alpha rotalpha1;                           //this is for rotating
+	protected static Alpha rotalpha2;                           //this is for rotating
+	protected static Alpha rotalpha3;                           //this is for rotating
+	protected static Alpha rotalpha4;                           //this is for rotating
+	protected static Alpha rotalpha5;                           //this is for rotating
+	protected static Alpha rotalpha6;                           //this is for rotating
+	protected static Alpha rotalpha7;                           //this is for rotating
+	protected static Alpha rotalpha8;                           //this is for rotating
+	protected static Alpha rotalpha9;                           //this is for rotating
 
+	// public Alpha get_Alpha() { return alpha; };    // NOTE: keep for future use 
+	private static RingObjectsKS sun;
+	private static RingObjectsKS mercury;
+	private static RingObjectsKS venus;
+	private static RingObjectsKS earth;
+	private static RingObjectsKS mars;
+	private static RingObjectsKS jupiter;
+	private static RingObjectsKS saturn;
+	private static RingObjectsKS uranus;
+	private static RingObjectsKS neptune;
+	private static RingObjectsKS pluto;
+
+	private boolean r_tag = true;
+	private boolean r = true;
+	
 	public static BranchGroup create_Scene() {
         Transform3D scaler = new Transform3D(); // 4x4 matrix for scaling
-		scaler.setScale(new Vector3d(0,0,.25f));
+		scaler.setScale(new Vector3d(0,0,1f));
 		BranchGroup sceneBG = new BranchGroup();           // create the scene' BranchGroup
 		TransformGroup sceneTG = new TransformGroup(); 
 		TransformGroup baseTG = new TransformGroup();   // create the scene's TransformGroup
@@ -33,22 +71,20 @@ public class Assignment3KS extends JPanel {
 
 		TransformGroup R1 = new TransformGroup();
 		TransformGroup cir  = new TransformGroup();
-		TransformGroup R3 = new TransformGroup();
-		TransformGroup R4 = new TransformGroup();
 
 		float x = (float) 0.9;
 
 		
-		RingObjectsKS sun = new Ring1(CommonsKS.Blue,(float)1,(float)0.0f);           // create the external object		
-		RingObjectsKS mercury = new Ring1(CommonsKS.White,(float)1,(float)x);           // create the external object		
-		RingObjectsKS venus = new Ring1(CommonsKS.White,(float)1,(float)2);           // create the external object		
-		RingObjectsKS earth = new Ring1(CommonsKS.White,(float)1,(float)3);           // create the external object		
-		RingObjectsKS mars = new Ring1(CommonsKS.White,(float)1,(float)4);           // create the external object		
-		RingObjectsKS jupiter = new Ring1(CommonsKS.White,(float)0.35,(float)5);           // create the external object		
-		RingObjectsKS saturn = new export("Saturn",CommonsKS.White,(float)1,(float)0.0,(float)0.0f,(float)6);           // create the external object		
-		RingObjectsKS uranus = new Ring1(CommonsKS.White,(float)1,(float)7);           // create the external object		
-		RingObjectsKS neptune = new Ring1(CommonsKS.White,(float)1,(float)8);           // create the external object		
-		RingObjectsKS pluto = new Ring1(CommonsKS.White,(float)1,(float)9);           // create the external object		
+		sun = new Ring1(CommonsKS.Blue,(float)1,(float)0.0f);           // create the external object		
+		mercury = new Ring1(CommonsKS.White,(float)1,(float)x);           // create the external object		
+		venus = new Ring1(CommonsKS.White,(float)1,(float)2);           // create the external object		
+		earth = new Ring1(CommonsKS.White,(float)1,(float)3);           // create the external object		
+		mars = new Ring1(CommonsKS.White,(float)1,(float)4);           // create the external object		
+		jupiter = new Ring1(CommonsKS.White,(float)0.35,(float)5);           // create the external object		
+		saturn = new export("Saturn",CommonsKS.White,(float)1,(float)0.0,(float)0.0f,(float)6);           // create the external object		
+		uranus = new Ring1(CommonsKS.White,(float)1,(float)7);           // create the external object		
+		neptune = new Ring1(CommonsKS.White,(float)1,(float)8);           // create the external object		
+		pluto = new Ring1(CommonsKS.White,(float)1,(float)9);           // create the external object		
 		
 		
 		TransformGroup sunTG= new TransformGroup();
@@ -63,37 +99,40 @@ public class Assignment3KS extends JPanel {
 		TransformGroup plutoTG = new TransformGroup();
 		
 		
+		alpha1 =new Alpha(-1,5000);
+		rotalpha1 = new Alpha(-1,2500) ;
 
-	
+		
 		sunTG.addChild(sun.position_Object());     
 		           
 		mercuryTG.addChild(mercury.position_Object());   
 
-		mercuryTG.addChild(CommonsKS.rotate_Behavior(50000,mercuryTG));
-		mercuryTG.addChild(CommonsKS.rotating(40000, mercuryTG));             
+		mercuryTG.addChild(CommonsKS.rotate_Behavior(50000,mercuryTG,alpha1));
+		mercuryTG.addChild(CommonsKS.rotating(400, mercuryTG,rotalpha1));             
+
 		venusTG.addChild(venus.position_Object());                // addding child ring1
-		venusTG.addChild(CommonsKS.rotate_Behavior(5000,venusTG));
+		venusTG.addChild(CommonsKS.rotate_Behavior(500,venusTG,alpha2));
 
 		earthTG.addChild(earth.position_Object());                // addding child ring1	
-		earthTG.addChild(CommonsKS.rotate_Behavior(5000,earthTG));
+		earthTG.addChild(CommonsKS.rotate_Behavior(4000,earthTG,alpha3));
 
 		marsTG.addChild(mars.position_Object());                // addding child ring1	
-		marsTG.addChild(CommonsKS.rotate_Behavior(5000,marsTG));
+		marsTG.addChild(CommonsKS.rotate_Behavior(5000,marsTG,alpha4));
 
 		jupiterTG.addChild(jupiter.position_Object());                // addding child ring1	
-		jupiterTG.addChild(CommonsKS.rotate_Behavior(5000,jupiterTG));
+		jupiterTG.addChild(CommonsKS.rotate_Behavior(5000,jupiterTG,alpha5));
 
 		saturnTG.addChild(saturn.position_Object());                // addding child ring1	
-		saturnTG.addChild(CommonsKS.rotate_Behavior(5000,saturnTG));
+		saturnTG.addChild(CommonsKS.rotate_Behavior(5000,saturnTG,alpha6));
 
 		uranusTG.addChild(uranus.position_Object());                // addding child ring1	
-		uranusTG.addChild(CommonsKS.rotate_Behavior(5000,uranusTG));
+		uranusTG.addChild(CommonsKS.rotate_Behavior(30000,uranusTG,alpha7));
 
 		neptuneTG.addChild(neptune.position_Object());                // addding child ring1	
-		neptuneTG.addChild(CommonsKS.rotate_Behavior(5000,neptuneTG));
+		neptuneTG.addChild(CommonsKS.rotate_Behavior(40000,neptuneTG,alpha8));
 
 		plutoTG.addChild(pluto.position_Object());                // addding child ring1	
-		plutoTG.addChild(CommonsKS.rotate_Behavior(5000,plutoTG));
+		plutoTG.addChild(CommonsKS.rotate_Behavior(50000,plutoTG,alpha9));
 
 		
 		R1.addChild(sunTG);
@@ -137,7 +176,7 @@ public class Assignment3KS extends JPanel {
 		sceneBG.addChild(R1);
 		sceneBG.addChild(sceneTG);
 		sceneBG.addChild(CommonsKS.add_Lights(CommonsKS.White, 1));	
-		sceneBG.addChild(CommonsKS.rotate_Behavior(6000, sceneTG));
+		// sceneBG.addChild(CommonsKS.rotate_Behavior(6000, sceneTG,alpha));
 		sceneBG.addChild(CommonsKS.create_BK(CommonsKS.Grey, b));		//creating background
 		
 		return sceneBG;
@@ -146,6 +185,7 @@ public class Assignment3KS extends JPanel {
 	public Assignment3KS(BranchGroup sceneBG) {
 		GraphicsConfiguration config = SimpleUniverse.getPreferredConfiguration();
 		Canvas3D canvas = new Canvas3D(config);
+		canvas.addKeyListener(this);
 		
 		SimpleUniverse su = new SimpleUniverse(canvas);    // create a SimpleUniverse
 		CommonsKS.define_Viewer(su, new Point3d(4.0d, 0.0d, 1.0d));
@@ -165,4 +205,41 @@ public class Assignment3KS extends JPanel {
 		frame.getContentPane().add(new Assignment3KS(create_Scene()));  // create an instance of the class
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
+
+				//Interaction using keyboard
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if ((e.getKeyCode() == KeyEvent.VK_X)){
+			if (r_tag) {
+				alpha1.pause();
+				// object3D[2].get_Alpha().pause();
+				// object3D[3].get_Alpha().pause();
+				// object3D[4].get_Alpha().pause();
+				// object3D[5].get_Alpha().pause();
+				r_tag = false;
+
+				}
+		else {
+			r_tag = true;
+			alpha1.resume();
+
+			}
+		}
+
+		else if(e.getKeyCode() == KeyEvent.VK_Z){
+			if (r) {
+				rotalpha1.pause();
+				r = false;
+			}	
+			else{
+				r =true;
+				rotalpha1.resume();
+
+			}
+		}
+	}
+		
+	public void keyTyped(KeyEvent e) {}
+	public void keyReleased(KeyEvent e) {}
+
 }
