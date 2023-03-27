f30ecf8143b8db48b988a15c773df50d85fa09c0
Kanishk Saini <117044498+Kanishk1604@users.noreply.github.com>
2023-03-26 20:45:44 -0400

    added revolutio nof planets

--- Assignment3KS.java
+++ Assignment3KS.java
@@ -58,9 +58,18 @@ public class Assignment3KS extends JPanel implements KeyListener {
 	private static RingObjectsKS neptune;
 	private static RingObjectsKS pluto;
 
-	private boolean r_tag = true;
-	private boolean r = true;
+	private boolean y = true;
+	private boolean v = true;
+	private boolean er = true;
+	private boolean m = true;
+	private boolean j = true;
+	private boolean s = true;
+	private boolean u = true;
+	private boolean n = true;
+	private boolean p = true;
 	
+	private boolean a = true;
+
 	public static BranchGroup create_Scene() {
         Transform3D scaler = new Transform3D(); // 4x4 matrix for scaling
 		scaler.setScale(new Vector3d(0,0,1f));
@@ -88,54 +97,117 @@ public class Assignment3KS extends JPanel implements KeyListener {
 		
 		
 		TransformGroup sunTG= new TransformGroup();
+
 		TransformGroup mercuryTG = new TransformGroup();
+		TransformGroup mercuryTG2 = new TransformGroup();
+
 		TransformGroup venusTG = new TransformGroup();
+		TransformGroup venusTG2 = new TransformGroup();
+		
 		TransformGroup earthTG = new TransformGroup();
+		TransformGroup earthTG2 = new TransformGroup();
+		
 		TransformGroup marsTG = new TransformGroup();
+		TransformGroup marsTG2 = new TransformGroup();
+		
 		TransformGroup jupiterTG = new TransformGroup();
+		TransformGroup jupiterTG2 = new TransformGroup();
+		
 		TransformGroup saturnTG = new TransformGroup();
+		TransformGroup saturnTG2 = new TransformGroup();
+		
 		TransformGroup uranusTG = new TransformGroup();
+		TransformGroup uranusTG2 = new TransformGroup();
+		
 		TransformGroup neptuneTG = new TransformGroup();
+		TransformGroup neptuneTG2 = new TransformGroup();
+		
 		TransformGroup plutoTG = new TransformGroup();
+		TransformGroup plutoTG2 = new TransformGroup();
 		
 		
 		alpha1 =new Alpha(-1,5000);
-		rotalpha1 = new Alpha(-1,2500) ;
+		alpha2 =new Alpha(-1,5000);
+		alpha3 =new Alpha(-1,5000);
+		alpha4 =new Alpha(-1,5000);
+		alpha5 =new Alpha(-1,5000);
+		alpha6 =new Alpha(-1,5000);
+		alpha7 =new Alpha(-1,5000);
+		alpha8 =new Alpha(-1,5000);
+		alpha9 =new Alpha(-1,5000);
 
-		
+		rotalpha1 = new Alpha(-1,2500) ;
+		rotalpha2 = new Alpha(-1,2500) ;
+		rotalpha3 = new Alpha(-1,2500) ;
+		rotalpha4 = new Alpha(-1,2500) ;
+		rotalpha5 = new Alpha(-1,2500) ;
+		rotalpha6 = new Alpha(-1,2500) ;
+		rotalpha7 = new Alpha(-1,2500) ;
+		rotalpha8 = new Alpha(-1,2500) ;
+		rotalpha9 = new Alpha(-1,2500) ;
+
+
 		sunTG.addChild(sun.position_Object());     
 		           
 		mercuryTG.addChild(mercury.position_Object());   
-
 		mercuryTG.addChild(CommonsKS.rotate_Behavior(50000,mercuryTG,alpha1));
-		mercuryTG.addChild(CommonsKS.rotating(400, mercuryTG,rotalpha1));             
+		mercuryTG2.addChild(mercury.position_Object());
+		mercuryTG2.addChild(CommonsKS.rotating(400, mercuryTG2,rotalpha1,(float)0.9));             
 
 		venusTG.addChild(venus.position_Object());                // addding child ring1
+		venusTG2.addChild(venus.position_Object());                // addding child ring1
 		venusTG.addChild(CommonsKS.rotate_Behavior(500,venusTG,alpha2));
+		venusTG2.addChild(CommonsKS.rotating(400, venusTG2,rotalpha2,(float)2));             
 
 		earthTG.addChild(earth.position_Object());                // addding child ring1	
+		earthTG2.addChild(earth.position_Object());                // addding child ring1	
 		earthTG.addChild(CommonsKS.rotate_Behavior(4000,earthTG,alpha3));
+		earthTG2.addChild(CommonsKS.rotating(400, earthTG2,rotalpha3,(float)3));             
 
 		marsTG.addChild(mars.position_Object());                // addding child ring1	
+		marsTG.addChild(mars.position_Object());                // addding child ring1	
 		marsTG.addChild(CommonsKS.rotate_Behavior(5000,marsTG,alpha4));
+		marsTG2.addChild(CommonsKS.rotating(400, marsTG2,rotalpha4,(float)4));             
 
 		jupiterTG.addChild(jupiter.position_Object());                // addding child ring1	
+		jupiterTG2.addChild(jupiter.position_Object());                // addding child ring1	
 		jupiterTG.addChild(CommonsKS.rotate_Behavior(5000,jupiterTG,alpha5));
+		jupiterTG2.addChild(CommonsKS.rotating(400, jupiterTG2,rotalpha5,(float)5));             
 
 		saturnTG.addChild(saturn.position_Object());                // addding child ring1	
+		saturnTG2.addChild(saturn.position_Object());                // addding child ring1	
 		saturnTG.addChild(CommonsKS.rotate_Behavior(5000,saturnTG,alpha6));
+		saturnTG2.addChild(CommonsKS.rotating(400, saturnTG2,rotalpha6,(float)6));             
 
 		uranusTG.addChild(uranus.position_Object());                // addding child ring1	
+		uranusTG2.addChild(uranus.position_Object());                // addding child ring1	
 		uranusTG.addChild(CommonsKS.rotate_Behavior(30000,uranusTG,alpha7));
+		uranusTG2.addChild(CommonsKS.rotating(400, uranusTG2,rotalpha7,(float)7));             
 
 		neptuneTG.addChild(neptune.position_Object());                // addding child ring1	
+		neptuneTG2.addChild(neptune.position_Object());                // addding child ring1	
 		neptuneTG.addChild(CommonsKS.rotate_Behavior(40000,neptuneTG,alpha8));
+		neptuneTG2.addChild(CommonsKS.rotating(400, neptuneTG2,rotalpha8,(float)8));             
 
 		plutoTG.addChild(pluto.position_Object());                // addding child ring1	
+		plutoTG2.addChild(pluto.position_Object());                // addding child ring1	
 		plutoTG.addChild(CommonsKS.rotate_Behavior(50000,plutoTG,alpha9));
+		plutoTG2.addChild(CommonsKS.rotating(400, plutoTG2,rotalpha9,(float)9));            
 
 		
 		R1.addChild(sunTG);
+
+		mercuryTG.addChild(mercuryTG2);
+		venusTG.addChild(venusTG2);
+		earthTG.addChild(earthTG2);
+		marsTG.addChild(marsTG2);
+		jupiterTG.addChild(jupiterTG2);
+		saturnTG.addChild(saturnTG2);
+		uranusTG.addChild(uranusTG2);
+		neptuneTG.addChild(neptuneTG2);
+		plutoTG.addChild(plutoTG2);
+	
+		
 		R1.addChild(mercuryTG);
 		R1.addChild(venusTG);
 		R1.addChild(earthTG);
@@ -209,30 +281,123 @@ public class Assignment3KS extends JPanel implements KeyListener {
 				//Interaction using keyboard
 	@Override
 	public void keyPressed(KeyEvent e) {
-		if ((e.getKeyCode() == KeyEvent.VK_X)){
-			if (r_tag) {
+		if ((e.getKeyCode() == KeyEvent.VK_Y)){
+			if (y) {
 				alpha1.pause();
-				// object3D[2].get_Alpha().pause();
-				// object3D[3].get_Alpha().pause();
-				// object3D[4].get_Alpha().pause();
-				// object3D[5].get_Alpha().pause();
-				r_tag = false;
+				y = false;
 
 				}
-		else {
-			r_tag = true;
-			alpha1.resume();
+			else {
+				y = true;
+				alpha1.resume();
+
+			}
+		}
+		if ((e.getKeyCode() == KeyEvent.VK_V)){
+			if (v) {
+				alpha2.pause();
+				v = false;
+
+				}
+			else {
+				v = true;
+				alpha2.resume();
+
+			}
+		}
+		if ((e.getKeyCode() == KeyEvent.VK_E)){
+			if (er) {
+				alpha3.pause();
+				er = false;
+
+				}
+			else {
+				er = true;
+				alpha3.resume();
+
+			}
+		}
+		if ((e.getKeyCode() == KeyEvent.VK_M)){
+			if (m) {
+				alpha4.pause();
+				m = false;
+
+				}
+			else {
+				m = true;
+				alpha4.resume();
+
+			}
+		}
+		if ((e.getKeyCode() == KeyEvent.VK_J)){
+			if (j) {
+				alpha5.pause();
+				m = false;
+
+				}
+			else {
+				j = true;
+				alpha5.resume();
+
+			}
+		}
+		if ((e.getKeyCode() == KeyEvent.VK_S)){
+			if (s) {
+				alpha6.pause();
+				s = false;
+
+				}
+			else {
+				s = true;
+				alpha6.resume();
+
+			}
+		}
+		if ((e.getKeyCode() == KeyEvent.VK_U)){
+			if (u) {
+				alpha7.pause();
+				u = false;
+
+				}
+			else {
+				u = true;
+				alpha7.resume();
 
 			}
 		}
 
-		else if(e.getKeyCode() == KeyEvent.VK_Z){
-			if (r) {
+
+		if ((e.getKeyCode() == KeyEvent.VK_N)){
+			if (n) {
+				alpha8.pause();
+				n = false;
+
+				}
+			else {
+				n = true;
+				alpha8.resume();
+
+			}
+		}
+		if ((e.getKeyCode() == KeyEvent.VK_P)){
+			if (p) {
+				alpha9.pause();
+				p = false;
+
+				}
+			else {
+				p = true;
+				alpha9.resume();
+
+			}
+		}
+		if(e.getKeyCode() == KeyEvent.VK_A){
+			if (a) {
 				rotalpha1.pause();
-				r = false;
+				a = false;
 			}	
 			else{
-				r =true;
+				a =true;
 				rotalpha1.resume();
 
 			}
