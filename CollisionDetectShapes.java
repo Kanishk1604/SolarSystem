a76335b14078817aae797def8a0ddb06c0315c67
Kanishk Saini <117044498+Kanishk1604@users.noreply.github.com>
2023-03-28 22:58:28 -0400

    Update CollisionDetectShapes.java

--- CollisionDetectShapes.java
+++ CollisionDetectShapes.java
@@ -2,16 +2,32 @@
 
 package solar;
 
+import java.awt.BorderLayout;
+import java.awt.GraphicsConfiguration;
+import java.io.FileNotFoundException;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.xml.crypto.dsig.Transform;
+
+import org.jogamp.java3d.*;
+import org.jogamp.java3d.loaders.IncorrectFormatException;
+import org.jogamp.java3d.loaders.ParsingErrorException;
+import org.jogamp.java3d.loaders.Scene;
+import org.jogamp.java3d.loaders.objectfile.ObjectFile;
+import org.jogamp.java3d.utils.geometry.Sphere;
+import org.jogamp.java3d.utils.image.TextureLoader;
+import org.jogamp.java3d.utils.picking.PickResult;
+import org.jogamp.java3d.utils.picking.PickTool;
+import org.jogamp.java3d.utils.universe.SimpleUniverse;
+import org.jogamp.vecmath.*;
+import org.jogamp.java3d.utils.universe.Viewer;
+import java.net.URL;
 import java.util.Iterator;
 
-import org.jogamp.java3d.Appearance;
-import org.jogamp.java3d.Behavior;
-import org.jogamp.java3d.ColoringAttributes;
-import org.jogamp.java3d.Shape3D;
-import org.jogamp.java3d.WakeupCriterion;
-import org.jogamp.java3d.WakeupOnCollisionEntry;
-import org.jogamp.java3d.WakeupOnCollisionExit;
-import org.jogamp.vecmath.Color3f;
+import org.jdesktop.j3d.examples.sound.PointSoundBehavior;
+import org.jdesktop.j3d.examples.sound.audio.JOALMixer;
+import java.awt.event.KeyEvent;
 
 /* This behavior of collision detection highlights the
     object when it is in a state of collision. */
@@ -22,12 +38,27 @@ public class CollisionDetectShapes extends Behavior {
 	private Appearance shapeAppearance;
 	private WakeupOnCollisionEntry wEnter;
 	private WakeupOnCollisionExit wExit;
+	private boolean ON;
+	private static TransformGroup t;
+	private static Transform3D trf;
+    public static Boolean cr = false;
+
+	// private static PointSound ps;
+	public Boolean getCr() {
+        return cr;
+    }
+
+    public void setCr(Boolean cr) {
+        this.cr = cr;
+    }
 
 	public CollisionDetectShapes(Shape3D s) {
 		shape = s; // save the original color of 'shape"
 		shapeAppearance = shape.getAppearance();
+		// this.ON = ON;
 		shapeColoring = shapeAppearance.getColoringAttributes();
 		inCollision = false;
+
 	}
 
 	@Override
@@ -39,12 +70,13 @@ public class CollisionDetectShapes extends Behavior {
 
 	@Override
 	public void processStimulus(Iterator<WakeupCriterion> criteria) {
-		Color3f hilightClr = CommonsKS.Green;
+		Color3f hilightClr = CommonsKS.Black;
 		ColoringAttributes highlight = new ColoringAttributes(hilightClr, ColoringAttributes.SHADE_GOURAUD);
 		inCollision = !inCollision; // collision has taken place
 
 		if (inCollision) { // change color to highlight 'shape'
 			shapeAppearance.setColoringAttributes(highlight);
+			cr = true;
 			wakeupOn(wExit); // keep the color until no collision
 		} else { // change color back to its original
 			shapeAppearance.setColoringAttributes(shapeColoring);
