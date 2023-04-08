41f8f44aedc2a83dfbe707de5287e948175bc3c9
Kanishk Saini <117044498+Kanishk1604@users.noreply.github.com>
2023-04-08 00:33:19 -0400

    Update CollisionDetectShapes.java

--- CollisionDetectShapes.java
+++ CollisionDetectShapes.java
@@ -2,32 +2,19 @@
 
 package solar;
 
-import java.awt.BorderLayout;
-import java.awt.GraphicsConfiguration;
-import java.io.FileNotFoundException;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.xml.crypto.dsig.Transform;
-
-import org.jogamp.java3d.*;
-import org.jogamp.java3d.loaders.IncorrectFormatException;
-import org.jogamp.java3d.loaders.ParsingErrorException;
-import org.jogamp.java3d.loaders.Scene;
-import org.jogamp.java3d.loaders.objectfile.ObjectFile;
-import org.jogamp.java3d.utils.geometry.Sphere;
-import org.jogamp.java3d.utils.image.TextureLoader;
-import org.jogamp.java3d.utils.picking.PickResult;
-import org.jogamp.java3d.utils.picking.PickTool;
-import org.jogamp.java3d.utils.universe.SimpleUniverse;
-import org.jogamp.vecmath.*;
-import org.jogamp.java3d.utils.universe.Viewer;
-import java.net.URL;
 import java.util.Iterator;
 
-import org.jdesktop.j3d.examples.sound.PointSoundBehavior;
-import org.jdesktop.j3d.examples.sound.audio.JOALMixer;
-import java.awt.event.KeyEvent;
+import org.jogamp.java3d.Appearance;
+import org.jogamp.java3d.Behavior;
+import org.jogamp.java3d.ColoringAttributes;
+import org.jogamp.java3d.Shape3D;
+import org.jogamp.java3d.Transform3D;
+import org.jogamp.java3d.TransformGroup;
+import org.jogamp.java3d.WakeupCriterion;
+import org.jogamp.java3d.WakeupOnCollisionEntry;
+import org.jogamp.java3d.WakeupOnCollisionExit;
+import org.jogamp.vecmath.Color3f;
+import org.jogamp.vecmath.Vector3d;
 
 /* This behavior of collision detection highlights the
     object when it is in a state of collision. */
@@ -38,12 +25,24 @@ public class CollisionDetectShapes extends Behavior {
 	private Appearance shapeAppearance;
 	private WakeupOnCollisionEntry wEnter;
 	private WakeupOnCollisionExit wExit;
-	private boolean ON;
-	private static TransformGroup t;
-	private static Transform3D trf;
-    public static Boolean cr = false;
-
-	// private static PointSound ps;
+	 private static SoundUtilityJOAL sound1;               
+	 private static TransformGroup t =null;
+	public CollisionDetectShapes(Shape3D s,SoundUtilityJOAL ss) {
+		shape = s; // save the original color of 'shape"
+		shapeAppearance = shape.getAppearance();
+		shapeColoring = shapeAppearance.getColoringAttributes();
+		inCollision = false;
+		sound1 = ss;
+	}
+	public static void playSound() {
+        String snd_pt = "crash";
+         
+        sound1.play(snd_pt);
+        try {
+            Thread.sleep(2500); // sleep for 0.5 secs
+        } catch (InterruptedException ex) {}
+        sound1.stop(snd_pt);
+    }
 	public Boolean getCr() {
         return cr;
     }
@@ -51,36 +50,38 @@ public class CollisionDetectShapes extends Behavior {
     public void setCr(Boolean cr) {
         this.cr = cr;
     }
-
-	public CollisionDetectShapes(Shape3D s) {
-		shape = s; // save the original color of 'shape"
-		shapeAppearance = shape.getAppearance();
-		// this.ON = ON;
-		shapeColoring = shapeAppearance.getColoringAttributes();
-		inCollision = false;
-
-	}
+    public Boolean cr = false;
 
 	@Override
 	public void initialize() { // USE_GEOMETRY USE_BOUNDS
-		wEnter = new WakeupOnCollisionEntry(shape, WakeupOnCollisionEntry.USE_BOUNDS);
-		wExit = new WakeupOnCollisionExit(shape, WakeupOnCollisionExit.USE_BOUNDS);
+		wEnter = new WakeupOnCollisionEntry(shape, WakeupOnCollisionEntry.USE_GEOMETRY);
+		wExit = new WakeupOnCollisionExit(shape, WakeupOnCollisionExit.USE_GEOMETRY);
 		wakeupOn(wEnter); // initialize the behavior
 	}
 
 	@Override
 	public void processStimulus(Iterator<WakeupCriterion> criteria) {
-		Color3f hilightClr = CommonsKS.Black;
+		Color3f hilightClr = CommonsKS.Green;
+		Color3f hilight = CommonsKS.Black;
 		ColoringAttributes highlight = new ColoringAttributes(hilightClr, ColoringAttributes.SHADE_GOURAUD);
+		ColoringAttributes highlight2 = new ColoringAttributes(hilight, ColoringAttributes.SHADE_GOURAUD);
+
+        // sound1 = new SoundUtilityJOAL();
 		inCollision = !inCollision; // collision has taken place
 
 		if (inCollision) { // change color to highlight 'shape'
 			shapeAppearance.setColoringAttributes(highlight);
+			// playSound();
 			cr = true;
 			wakeupOn(wExit); // keep the color until no collision
-		} else { // change color back to its original
-			shapeAppearance.setColoringAttributes(shapeColoring);
-			wakeupOn(wEnter); // wait for collision happens
+		 } 
+		 else { // change color back to its original
+			// shapeAppearance.setColoringAttributes(shapeColoring);
+			// wakeupOn(wEnter); // wait for collision happens
+			playSound();
+
+			shapeAppearance.setColoringAttributes(highlight2);
+
 		}
 	}
 }
